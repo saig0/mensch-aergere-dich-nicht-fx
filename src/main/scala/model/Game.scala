b22882b7a07b5dc3c5455d7f8dfecbae633b9cec
Philipp <philipp.ossler@gmail.com>
2013-05-10 18:24:15 +0200

    REST-API abfragen und nach JSON parsen

---
+++ src/main/scala/model/Game.scala
@@ -0,0 +1,3 @@
+package model
+
+case class Game(id: Long, address: String, maxPlayers: Int, currentPlayers: Int)
--- src/main/scala/test/Main.scala
+++ src/main/scala/test/Main.scala
@@ -2,17 +2,25 @@ package test
 
 import dispatch._
 import Defaults._
+import net.liftweb.json._
+import net.liftweb.json.Serialization._
+import model.Game
 
 object Main {
+
+	implicit val formats = DefaultFormats
 
 	def main(args: Array[String]) {
 		println("Hallo")
 
 		val request = host("rest-game-server.herokuapp.com") / "games" GET
 		val result = Http(request OK as.String)
-		val games = result()
-		println(games)
 
-		Thread.sleep(5000)
+		val json = parse(result())
+		val games = json.extract[List[Game]]
+
+		games map println
+
+		println(write(games))
 	}
 }
